06ddfbad5d240058f3767b27f779ba630ff94790
Your Name <you@example.com>
2021-05-06 00:03:53 -0400

    Hello!!! Welcome to first github demo class

---
+++ src/Demo1.java
@@ -0,0 +1,13 @@
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x =1;
+int y = 2;
+int sum = x + y;
+;
+System.out.println(" Sum of two numbers");
+
+	}
+
+}
---
+++ src/Demo2/Demo2.java
@@ -0,0 +1,15 @@
+package Demo2;
+
+public class Demo2 {
+
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	
+		System.out.println(" Sum of two numbers");
+
+
+
+	}
+
+}
